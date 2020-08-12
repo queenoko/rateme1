391b1feffd4d874ddfbbaf52c11573d84edb2076
queenoko <quimaudongwo@gmail.com>
2020-08-12 18:08:41 +0100

    upload file

--- routes/company.js
+++ routes/company.js
@@ -10,6 +10,27 @@ module.exports = (app) => {
 
     //ROUTE FOR UPLOAD
     app.post('/upload', (req, res) => {
-        
-    })
+        var form = new formidable.IncomingForm(); // incoming form object
+
+        form.uploadDir = path.join(__dirname, '../public/uploads');
+
+        form.on('file', (field, file) => { // to rename a file
+            fs.rename(file.path, path.join(form.uploadDir, file.name), (err) => {
+                if(err){
+                    throw err
+                }
+
+                console.log('File has been renamed');
+
+            });
+        });
+
+        form.on('error', (err) => {
+            console.log('An error has occured', err);
+        });
+        form.on('end', () => {
+            console.log('File upload was successfull');
+        });
+        form.parse(req);
+    });
 }
